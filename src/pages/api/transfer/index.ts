aa5b2827de20ff73c0973a4e9b487e074cb5234f
Sbsbsv <sbsbsv002@gmail.com>
2024-04-11 10:00:55 +0530

    Transaction updated

--- src/pages/api/transfer/index.ts
+++ src/pages/api/transfer/index.ts
@@ -2,25 +2,38 @@ import type { NextApiRequest, NextApiResponse } from 'next'
  import { verifyToken } from '@/lib/fetchuser'
 import AccountBalance from '@/models/accountSchema'
 import mongoose from 'mongoose'
+import User from '@/models/User'
 export default async function transfer(req: NextApiRequest, res: NextApiResponse) {
     const userToken=req.cookies['next-auth.session-token']
     if(!userToken){
         return res.status(401).json({error: "Please login"})
     }
     const userData=await verifyToken(userToken)
-    console.log(userData.user._id)
     if(!userData.user)
     return res.status(userData.status?userData.status:500).json({message:userData.message})
+    const userId=userData.user._id
 
     try {
-        const {amount,to}=req.body;
-        if(!amount || !to){
-        }
+        // const {amount,to}=req.body;
+        // if(!amount || !to){
+        //     return res.status(400).json({message:"Insufficient Data"})
+        // }
+        
+        const to="param11650@gmail.com"
        const session=await mongoose.startSession();
         session.startTransaction();
+        console.log(userId)
+        const account =await AccountBalance.findOne({userId:userId}).session(session);
+        console.log("here")
+        const toUser =await User.findOne({userId:userId}).session(session);
+        console.log("there")
+        const toAccount =await AccountBalance.findOne({userId:toUser}).session(session);
+        console.log(toAccount)
 
-       return res.status(200).json({})
+        
+       return res.status(200).json({mesage:"sucess"})
     } catch (error) {
+        console.log(error)
         return res.status(500).json({message:(error as Error).message})
     }
 
