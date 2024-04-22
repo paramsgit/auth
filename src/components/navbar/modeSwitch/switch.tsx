4fdcc1400e1272c32a73a6f308b5f011fed2f7de
param <param11650@gmail.com>
2024-04-22 13:06:10 +0530

    Errors Resolved

--- src/components/navbar/modeSwitch/switch.tsx
+++ src/components/navbar/modeSwitch/switch.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { useTheme } from 'next-themes';
-
+import { useEffect } from 'react';
 export default function Switcher() {
   const {theme, setTheme} = useTheme()
+  const [darkMode, setdarkMode] = useState(false)
+    useEffect(() => {
+    if(theme=="dark"){
+      setdarkMode(true)
+    } 
+    }, [theme])
     
 
   return (
     <>
        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-        {theme}
+        { darkMode?"light":"dark"}
+
       </button>
     </>
   );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -21,12 +21,12 @@ export const Navbar = (props: Props) => {
     <div>
   <Switcher/>
   </div>
-      <button onClick={()=>signOut()} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-2xl md:rounded-lg text-xs md:text-sm px-3 py-2 md:px-4 md:py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <button onClick={()=>signOut()} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg sm:rounded-2xl md:rounded-lg text-xs md:text-sm px-3 py-2 md:px-4 md:py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         
        <span className='hidden sm:block'> Sign Out</span>
-       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right " viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
-  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right sm:hidden" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+  <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
 </svg>
       </button>
 
