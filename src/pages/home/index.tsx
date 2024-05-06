c43a07e96e0f233b79abb547101444c2c5cf6282
param <param11650@gmail.com>
2024-05-06 14:59:10 +0530

    Update: Fixing buttons for home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -48,7 +48,7 @@ const Front: React.FunctionComponent<IFrontProps> = (props) => {
         </div>
         <div className='MoneyButtons px-4 md:px-10 flex '>
         <button onClick={showTrsForm} type="button" className="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 min-w-[120px]">{!TrsForm? "Send Money":"Hide"}</button>
-        <button type="button" className="mx-3 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10  focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Receive Money</button>
+        <button type="button" className="mx-3 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray-100 hover:bg-gray-200 rounded-lg border border-gray-200 hover:text-blue-700 focus:z-10  focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Receive Money</button>
 
         </div>
         <div className={` transactionForm px-4 md:p-1 lg:px-10 py-4 ${!TrsForm && 'hidden'}`}>
