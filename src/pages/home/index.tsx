c29e44bf812c7036c951f2655375bc70495274be
param <param11650@gmail.com>
2024-05-07 11:32:55 +0530

    Update: Qr Scanner Added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,6 @@ import TransactionForm from './transactionForm';
 import Graph from './graph';
 import SetUpPage from './setupPage';
 
-import io from 'socket.io-client';
 
 interface IFrontProps {
 }
@@ -51,6 +50,7 @@ const Front: React.FunctionComponent<IFrontProps> = (props) => {
         <button type="button" className="mx-3 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray-100 hover:bg-gray-200 rounded-lg border border-gray-200 hover:text-blue-700 focus:z-10  focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Receive Money</button>
 
         </div>
+        
         <div className={` transactionForm px-4 md:p-1 lg:px-10 py-4 ${!TrsForm && 'hidden'}`}>
             <TransactionForm fnss={updateBalance}/>
         </div>
--- src/pages/home/transactionForm.tsx
+++ src/pages/home/transactionForm.tsx
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import react, { useEffect, useState } from 'react';
+import { Scanner } from '@yudiel/react-qr-scanner';
 
 const numberToText = require('number-to-text')
 require('number-to-text/converters/en-us');
@@ -32,6 +33,7 @@ const TransactionForm: React.FunctionComponent<ITransactionFormProps> = ({fnss})
     const [inputFocused2,setinputFocused2]=useState(false)
     const [agreedCheck,setagreedCheck]=useState(false)
     const [isSubmitted,setisSubmitted]=useState(false)
+    const [isCameraOn,setisCameraOn]=useState(false)
     const [amountInWords,setamountInWords]=useState("")
     const router=useRouter();
     const queryClient=useQueryClient()
@@ -98,6 +100,24 @@ const TransactionForm: React.FunctionComponent<ITransactionFormProps> = ({fnss})
         if(am=="" || trueString)
         setamount(am)
     }
+    function isEmail(text:string) {
+      // Regular expression for email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(text);
+  }
+    const handleScan=(text?:string)=>{
+      if(text){
+        console.log(text)
+        if(isEmail(text)){
+          setReceiverId(text);
+          setisCameraOn(false)
+        }else{
+          console.log("Not valid text")
+
+        }
+      }
+    }
+
   return <>
   
 
@@ -125,21 +145,37 @@ const TransactionForm: React.FunctionComponent<ITransactionFormProps> = ({fnss})
   </li>})}
 </ul>
 </div>
-
+<div className='flex justify-center w-full my-4'>
+  <div className='w-[80%] bg-white dark:bg-gray-950/60 rounded-[1rem]'>
+  <div className='flex justify-center h-[80%] items-center absolute w-[80%]'>
+          {!isCameraOn
+          ?
+          <button type='button' onClick={()=>{setisCameraOn(true)}} className='z-[8] absolute text-gray-400 dark:text-gray-500 flex flex-col items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-camera" viewBox="0 0 16 16">
+  <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
+  <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+</svg>
+            Turn On</button>
+         :
+          <button type='button' onClick={()=>{setisCameraOn(false)}} className='z-[8] absolute top-[8px] right-[8px] w-7 h-7 rounded-full border-[1px] text-gray-200 border-gray-300 dark:text-gray-600 dark:border-gray-600'>x</button>
+          }
+        </div>
+      <Scanner styles={{ container:{width:"100%",opacity:`${isCameraOn?"1":"0"}`,borderRadius:"1rem"}}} enabled={isCameraOn} components={ {tracker: true}} options={{delayBetweenScanSuccess:1000}}
+                  onResult={(text, result) => {handleScan(text)}}
+                  onError={(error) => console.log(error?.message)}
+              />
+       
+</div>
+</div>
   </div>
   <div className="mb-5">
     <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
     <input type="text" id="amount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={amount} onChange={(e)=>{checkAndSetAmount(e.target.value)}} autoComplete='off' required />
   </div>
   
-  <div className="flex items-start mb-5">
-    <div className="flex items-center h-5">
-      <input id="remember" type="checkbox" checked={agreedCheck} onChange={()=> setagreedCheck(!agreedCheck)} className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"  required />
-    </div>
-    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Agree to Send {amountInWords ? `${amountInWords} Rupees`:""}</label>
-  </div>
+
   <div className='flex justify-center'>
-  <button type="submit" className={`${(!agreedCheck || !ReceiverId || !amount) && "disabled"} mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>{isSubmitted?"Proceeding":"Proceed"} <span className={`mx-1 text-yellow-300 hidden`}> 4</span>
+  <button type="submit" className={`${( !ReceiverId || !amount) && "disabled"} mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>{isSubmitted?"Proceeding":"Proceed"} <span className={`mx-1 text-yellow-300 hidden`}> 4</span>
   </button>
   </div>
 </form>
